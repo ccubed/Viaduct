b036dd01866eccd2a2d94890ededf86ddb5ff7f2
Cooper <ccubed.techno@gmail.com>
2015-11-08 19:26:55 -0500

    Stonework Error Handling. I forgot to update pairs.

--- viaduct/Stonework.py
+++ viaduct/Stonework.py
@@ -13,53 +13,65 @@ class Stonework:
 
     # Add data to a hash or edit what's there
     def addhash(self, hashid, data, db):
-        if hashid == 0:
-            return 0
+        if 15 <= db >= 0:
+            if hashid == 0:
+                return 0
+            else:
+                for (k, v) in data:
+                    self.rc[db][2].hset(hashid, k, v)
+                if db == 2:
+                    # Comms are only saved for 48 hours
+                    self.rc[db][2].expire(hashid, 172800)
+                elif 10 <= db < 15:
+                    # The log DBs keep log entries for 4 days
+                    self.rc[db][2].expire(hashid, 345600)
+                elif db == 15:
+                    # The dbus keeps messages for 1 hour
+                    self.rc[db][2].expire(hashid, 3600)
+                return 1
         else:
-            for (k, v) in data:
-                self.rc[db][2].hset(hashid, k, v)
-            if db == 2:
-                # Comms are only saved for 48 hours
-                self.rc[db][2].expire(hashid, 172800)
-            elif 10 <= db < 15:
-                # The log DBs keep log entries for 4 days
-                self.rc[db][2].expire(hashid, 345600)
-            elif db == 15:
-                # The dbus keeps messages for 1 hour
-                self.rc[db][2].expire(hashid, 3600)
-            return 1
+            return 0
 
     # Create a key value pair. These are temporary and only exist in DB 8.
     def addpair(self, key, value, db):
-        self.rc[db][2].add(key, value)
-        return 1
+        if 15 <= db >= 0:
+            self.rc[db][2].add(key, value)
+            return 1
+        else:
+            return 0
 
     # Get data from a hash and return it in the requested format.
     def gethash(self, hashid, data, db, return_format='json'):
-        if return_format == 'json':
-            if data == 'all':
-                return json.dumps(self.rc[db][2].hgetall(hashid))
-            else:
-                return json.dumps(self.rc[db][2].hget(hashid,data))
-        elif return_format == 'python':
-            if data == 'all':
-                return self.rc[db][2].hgetall(hashid)
-            else:
-                return self.rc[db][2].hget(hashid,data)
-        elif return_format == 'text':
-            if data == 'all':
-                unfdata = self.rc[db][2].hgetall(hashid)
-                retstring = ""
-                for (k,v) in unfdata:
-                    retstring = str(k) + ": " + str(v) + '\n'
-                return retstring
-            else:
-                unfdata = self.rc[db][2].hget(hashid,data)
-                return str(unfdata[0][0]) + ": " + str(unfdata[0][1])
+        if 15 <= db >= 0:
+            if return_format == 'json':
+                if data == 'all':
+                    return json.dumps(self.rc[db][2].hgetall(hashid))
+                else:
+                    return json.dumps(self.rc[db][2].hget(hashid,data))
+            elif return_format == 'python':
+                if data == 'all':
+                    return self.rc[db][2].hgetall(hashid)
+                else:
+                    return self.rc[db][2].hget(hashid,data)
+            elif return_format == 'text':
+                if data == 'all':
+                    unfdata = self.rc[db][2].hgetall(hashid)
+                    retstring = ""
+                    for (k,v) in unfdata:
+                        retstring = str(k) + ": " + str(v) + '\n'
+                    return retstring
+                else:
+                    unfdata = self.rc[db][2].hget(hashid,data)
+                    return str(unfdata[0][0]) + ": " + str(unfdata[0][1])
+        else:
+            return 'Error'
 
     # Get the value for a key and delete the key. Remember, temporary.
     def getpair(self, key, db):
-        return self.rc[db][2].get(key)
+        if 15 <= db >= 0:
+            return self.rc[db][2].get(key)
+        else:
+            return 'Error'
 
     # Dump redis to disk. This is an asynchronous task.
     def redisdump(self):
